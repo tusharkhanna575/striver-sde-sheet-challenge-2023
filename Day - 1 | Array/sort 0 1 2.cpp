7bd8378eb53d8a1c035bac410813c524c0c16588
Tushar Khanna <0808cs201190.ies@ipsacademy.org>
2023-06-05 23:55:35 +0530

    Create sort 0 1 2.cpp

---
+++ Day - 1 | Array/sort 0 1 2.cpp
@@ -0,0 +1,42 @@
+//code by @tusharkhanna
+#include<bits/stdc++.h>
+using namespace std;
+
+//brute
+void sort012(vector<int> &a) {
+    sort(a.begin(), a.end());
+}
+
+//optimal -> dutch national flag algorithm
+void sort_012(vector<int> &a) {
+    int low = 0, mid = 0, high = a.size()-1;
+    while(mid <= high) {
+        if(a[mid] == 0) {
+            swap(a[low++], a[mid++]);
+        }
+        else if(a[mid] == 1) {
+            mid++;
+        }
+        else {
+            swap(a[mid], a[high--]);
+        }
+    }
+}
+
+int main() {
+    int n;
+    vector<int> a(n);
+    for(int i: a) {
+        cin>>i;
+    }
+    cout<<"Given array : ";
+    for(int i: a) {
+        cout<<i<<" ";
+    }
+    sort_012(a);
+    cout<<endl<<"Sorted array : ";
+    for(int i: a) {
+        cout<<i<<" ";
+    }
+    return 0;
+}
